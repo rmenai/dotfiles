bea13c2ee0f65704062c827d4a56398393917134
rmenai <rami.menai@outlook.com>
2024-07-30 16:10:36 +0200

    Prevent builtin plugins from loading

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,8 +1,18 @@
 -- OPTIONS
--- To see what an option is set to execute `:lua = vim.o.<name>`
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
 
-vim.g.loaded_netrw = 1 -- Enable vim's builtin file explorer
-vim.g.loaded_netrwPlugin = 1 -- Load the netrw plugi`n
 vim.g.python3_host_prog = "/usr/bin/python" -- Specify python speed for faster loading
 
 vim.o.autoindent = true -- Automatically indent
