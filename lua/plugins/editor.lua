ecffc474409bea0ef178499609c9cd7809348436
rmenai <rami.menai@outlook.com>
2024-07-30 23:54:23 +0200

    Fix annoying esc from visual mode

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -177,7 +177,16 @@ return {
     config = function()
       require("houdini").setup({
         mappings = { "jj", "jk" },
-        timeout = 200
+        timeout = 200,
+        escape_sequences = {
+          ["v"] = false,
+          ["vs"] = false,
+          ["V"] = false,
+          ["Vs"] = false,
+          ["no"] = false,
+          ["nov"] = false,
+          ["noV"] = false,
+        }
       })
     end
   }
