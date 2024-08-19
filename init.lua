2718e199472fb52dc802a53b80f2036ef06903f4
rmenai <rami.menai@outlook.com>
2024-08-19 11:07:54 +0200

    Update init file

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
-
-
 -- External Dependencies
 -- sudo apt-get install ripgrep fd-find
 -- pip install python-lsp-server
 -- pip install --user klepto
+-- cargo install tree-sitter-cli
 -- sioyek
 
 -- Plugins
@@ -11,6 +10,8 @@ require("core.lazy")
 
 -- Core setup
 require("core.settings")
--- require("core.patches")
 require("core.commands")
 require("core.mappings")
+
+-- Set up clipboard
+require("core.autocmds").clipboard_autocmd()
