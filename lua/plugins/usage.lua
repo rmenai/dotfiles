b2bec7316be7ed5c7b8cd398fa9a8e421feb9614
rmenai <rami.menai@outlook.com>
2024-08-19 14:53:05 +0200

    Fix project.nvim not loading

--- lua/plugins/usage.lua
+++ lua/plugins/usage.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    -- AUTO CWD
-    "ahmedkhalf/project.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("project_nvim").setup({
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.py" }
-      })
-    end
-  }
+	{
+		-- AUTO CWD
+		"ahmedkhalf/project.nvim",
+		event = "BufRead",
+		config = function()
+			require("project_nvim").setup({
+				patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.py" },
+			})
+		end,
+	},
 }
