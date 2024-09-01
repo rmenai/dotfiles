b00f4d60705c0d76bcb34115eb0f624b0b2a4134
Rami Menai <rami.menai@outlook.com>
2024-09-01 17:47:56 +0200

    Switch to home-manager

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,79 @@
+{ pkgs, ... }:
+
+let
+  homeDir = builtins.getEnv "HOME";
+  user = builtins.getEnv "USER";
+  jsonFile = builtins.readFile "${homeDir}/.config/zsh/themes/catppuccin_mocha.omp.json";
+  themeConfig = builtins.fromJSON (builtins.unsafeDiscardStringContext jsonFile);
+in
+
+{
+  home = {
+    stateVersion = "24.05";
+    username = user;
+    homeDirectory = homeDir;
+    packages = with pkgs; [
+      zsh
+      oh-my-posh
+      neovim
+      tmux
+      
+      ripgrep
+      fd
+      fzf
+      python312
+      python312Packages.pip
+      python312Packages.cmake
+      nodejs_22
+      yarn
+      rustup
+      gnumake42
+      gccgo14
+      unzip
+
+      ccls
+      tree-sitter
+    ];
+    shellHook = ''
+      if ! python3.12 -m pip show klepto > /dev/null 2>&1; then
+        python3.12 -m pip install --user klepto
+      fi
+    '';
+    sessionVariables = {
+      EDITOR = "neovim";
+      SUDO_EDITOR = "neovim";
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      shellAliases = {
+        ll = "ls -alF";
+        la = "ls -A";
+        l = "ls -CF";
+      };
+      initExtra = ''
+        # Source Nix environment script if it exists
+        if [[ -r "${homeDir}/.profile" ]]; then
+          source "${homeDir}/.profile"
+        fi
+      '';
+    };
+    oh-my-posh = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = themeConfig;
+    };
+    git = {
+      enable = true;
+      userName = "Rami Menai";
+      userEmail = "rami@menai.me";
+    };
+  };
+}
+
