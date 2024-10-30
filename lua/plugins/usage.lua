8990604d67c99c014a880c80d3b8a234d21d8a30
Rami Menai <rami@menai.me>
2024-10-30 10:59:18 +0100

    Disable cycle navigation

--- lua/plugins/usage.lua
+++ lua/plugins/usage.lua
@@ -25,7 +25,18 @@ return {
   {
     "aserowy/tmux.nvim",
     config = function()
-      require("tmux").setup()
+      require("tmux").setup({
+        navigation = {
+          enable_default_keybindings = true,
+          cycle_navigation = false,
+          persist_zoom = false,
+        },
+        resize = {
+          enable_default_keybindings = true,
+          resize_step_x = 2,
+          resize_step_y = 2,
+        },
+      })
     end,
   },
 }
