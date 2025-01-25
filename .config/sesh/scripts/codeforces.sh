a02530ec36f883a6f34bac67ca4e40dadc8672b0
Rami Menai <rami@menai.me>
2025-01-25 22:44:21 +0100

    Add codeforces support

---
+++ .config/sesh/scripts/codeforces.sh
@@ -0,0 +1,9 @@
+SESSION_NAME="Codeforces"
+
+tmux send-keys "rust-competitive-helper" C-m
+tmux send-keys C-m
+
+tmux splitw -h
+
+tmux send-keys "rust-competitive-helper" C-m
+tmux send-keys Down
--- .config/sesh/scripts/school.sh
+++ .config/sesh/scripts/school.sh
@@ -1,10 +1,10 @@
 SESSION_NAME="School"
 
 tmux rename-window -t $SESSION_NAME:1 "C"
-tmux send-keys -t $SESSION_NAME:1 "cd ~/Documents/school/c/src" C-m
+tmux send-keys -t $SESSION_NAME:1 "cd ~/Documents/projects/school/c/src" C-m
 tmux send-keys -t $SESSION_NAME:1 "nvim test.c" C-m
 
-tmux new-window -t $SESSION_NAME:2 -n "Ocaml" -c ~/Documents/school/ocaml/bin
+tmux new-window -t $SESSION_NAME:2 -n "Ocaml" -c ~/Documents/projects/school/ocaml/bin
 tmux send-keys -t $SESSION_NAME:2 "nvim main.ml" C-m
 
 tmux select-window -t $SESSION_NAME:1
