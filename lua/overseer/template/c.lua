4e0d27fa5030a53fec60553817d421b5fe5faaa1
Rami Menai <rami@menai.me>
2025-02-17 23:37:45 +0100

    Add more c commands

--- lua/overseer/template/c.lua
+++ lua/overseer/template/c.lua
@@ -1,27 +1,48 @@
 return {
   condition = {
     callback = function()
-      if not vim.fn.executable("gcc") == 1 then return false, "Executable gcc not found" end
-      if vim.fn.expand("%:e") ~= "c" then return false, "No c file found" end
+      if vim.fn.executable("gcc") ~= 1 then return false, "Executable gcc not found" end
+      if vim.fn.expand("%:e") ~= "c" then return false, "No C file found" end
       return true
     end,
   },
   generator = function(_, cb)
     local templates = {
       {
-        name = "Run this program",
+        name = "Interpret this program",
+        builder = function()
+          local file = vim.fn.expand("%:p")
+          local executable = vim.fn.fnamemodify(file, ":r")
+          local command = string.format("gcc %s -o %s && %s && rm %s", file, executable, executable, executable)
+          return {
+            cmd = { "bash", "-c", command },
+          }
+        end,
+      },
+      {
+        name = "Compile this program",
         builder = function()
           local file = vim.fn.expand("%:p")
           local executable = vim.fn.fnamemodify(file, ":r")
           executable = "out/" .. vim.fn.fnamemodify(executable, ":t")
-
+          local command = string.format("mkdir -p out && gcc %s -o %s", file, executable)
           return {
-            cmd = { "gcc", file, "-o", executable, "&&", executable },
+            cmd = { "bash", "-c", command },
+          }
+        end,
+      },
+      {
+        name = "Run this program",
+        builder = function()
+          local file = vim.fn.expand("%:p")
+          local executable = "out/" .. vim.fn.fnamemodify(file, ":t:r")
+          local command = string.format("[ -f %s ] && %s || echo 'Executable not found. Compile first.'", executable, executable)
+          return {
+            cmd = { "bash", "-c", command },
           }
         end,
       },
     }
-
     cb(templates)
   end,
 }
