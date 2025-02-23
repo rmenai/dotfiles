a70a8fb962ab355a6ad492ad832dcbfb5105e0d1
Rami Menai <rami@menai.me>
2025-02-23 16:32:33 +0100

    Update config

--- .config/sesh/scripts/school.sh
+++ .config/sesh/scripts/school.sh
@@ -1,10 +1,10 @@
 SESSION_NAME="School"
 
-tmux rename-window -t $SESSION_NAME:1 "C"
-tmux send-keys -t $SESSION_NAME:1 "cd ~/Documents/projects/school/c/src" C-m
-tmux send-keys -t $SESSION_NAME:1 "nvim test.c" C-m
+tmux send-keys -t $SESSION_NAME:1 "cd ~/Documents/projects/school/cs/ocaml/bin" C-m
+tmux send-keys -t $SESSION_NAME:1 "nvim main.ml" C-m
 
-tmux new-window -t $SESSION_NAME:2 -n "Ocaml" -c ~/Documents/projects/school/ocaml/bin
-tmux send-keys -t $SESSION_NAME:2 "nvim main.ml" C-m
+tmux splitw -h
+tmux send-keys "utop" C-m
 
-tmux select-window -t $SESSION_NAME:1
+tmux new-window -t $SESSION_NAME:2 -n "Exercices" -c ~/Documents/projects/school/cs
+tmux send-keys -t $SESSION_NAME:2 "y" C-m
