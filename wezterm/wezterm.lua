b7d00ecdac0b32c609f6e508fb8adabd04927c06
Rami Menai <rami@menai.me>
2025-03-01 01:02:15 +0100

    Change a little

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font("JetBrains Mono"),
-  font_size = 12.0,
-  color_scheme = "Catppuccin Mocha",
-  window_background_opacity = 0.95,
-  hide_tab_bar_if_only_one_tab = true,
+	font = wezterm.font("JetBrains Mono Nerd Font"),
+	font_size = 12.0,
+	color_scheme = "Catppuccin Mocha",
+	window_background_opacity = 0.95,
+	hide_tab_bar_if_only_one_tab = true,
+	default_cursor_style = "SteadyBar",
 }
