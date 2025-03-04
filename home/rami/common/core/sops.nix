59c61bc140682f461d2ca7f2c303aba2db98cb53
Rami Menai <rami@menai.me>
2025-03-04 23:38:52 +0100

    Prefer dynamic home

--- home/rami/common/core/sops.nix
+++ home/rami/common/core/sops.nix
@@ -1,19 +1,22 @@
 {
   inputs,
   lib,
+  config,
+  hostSpec,
   ...
 }: let
   sopsFolder = builtins.toString inputs.nix-secrets;
+  homeDir = config.home.homeDirectory;
 in {
   sops = {
-    age.keyFile = "/home/rami/.config/sops/age/keys.txt";
+    age.keyFile = "${homeDir}/.config/sops/age/keys.txt";
 
     defaultSopsFile = "${sopsFolder}/secrets.yaml";
     validateSopsFiles = false;
 
     secrets = {
-      "private_keys/rami" = {
-        path = "/home/rami/.ssh/id_null";
+      "private_keys/${hostSpec.username}" = {
+        path = "${homeDir}/.ssh/id_null";
       };
     };
   };
