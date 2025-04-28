35fa2824f975e0dd5abcfdebbd89aea47fb8b9dd
Rami Menai <rami@menai.me>
2025-04-28 22:45:31 +0200

    Add virtualisation

--- hosts/common/optional/vm.nix
+++ hosts/common/optional/vm.nix
@@ -1,8 +1,31 @@
 {pkgs, ...}: {
   virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  # virtualisation.qemu.options = {
+  #   options = [
+  #     "-device"
+  #     "virtio-vga-gl"
+  #     "-display"
+  #     "sdl,gl=on,show-cursor=off"
+  #     "-device"
+  #     "virtio-keyboard"
+  #   ];
+  # };
 
   boot.kernelModules = ["kvm-intel" "vfio-pci"];
   boot.kernelParams = ["intel_iommu=on" "iommu=pt"];
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+  };
+
+  environment.persistence."/persist/system" = {
+    directories = [
+      "/var/lib/libvirt"
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     virt-manager
