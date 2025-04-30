6bb3dc98df0cf9042bf198f6897b247afd869d64
Rami Menai <rami@menai.me>
2025-04-30 03:27:42 +0200

    Fix tailscale

--- nixos/hosts/common/optional/services/tailscale.nix
+++ nixos/hosts/common/optional/services/tailscale.nix
@@ -1,7 +1,7 @@
-{
-  services.tailscale = {
-    enable = true;
-  };
+{pkgs, ...}: {
+  services.tailscale.enable = true;
+  networking.firewall.allowedUDPPorts = [41641];
+  environment.systemPackages = [pkgs.tailscale];
 
   environment.persistence."/persist/system" = {
     directories = [
