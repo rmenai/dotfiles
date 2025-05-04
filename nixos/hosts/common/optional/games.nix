321ccfa95f0ec24a98d42ba35e291a0a4308de15
Rami Menai <rami@menai.me>
2025-05-04 22:55:55 +0200

    Add environment

--- nixos/hosts/common/optional/games.nix
+++ nixos/hosts/common/optional/games.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   programs.gamemode.enable = true;
 
+  environment.sessionVariables = {
+    GAMEMODERUNEXEC = "nvidia-offload";
+  };
+
   environment.systemPackages = with pkgs; [
     steam-run
   ];
