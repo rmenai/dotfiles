cb7b81501281cc730bacdc2d4916882757fb2908
Rami Menai <rami@menai.me>
2025-05-07 13:27:42 +0200

    Change settings in tmux

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -30,3 +30,5 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+if not os.getenv("TMUX") then vim.o.cmdheight = 0 end
