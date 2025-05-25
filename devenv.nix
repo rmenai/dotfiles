99f036e790081a88a77bf4533ca99c33b1eef6f6
Rami Menai <rami@menai.me>
2025-05-25 11:56:52 +0200

    fix: remove unecessary key

--- devenv.nix
+++ devenv.nix
@@ -10,7 +10,7 @@
     check-yaml.enable = true;
     commitizen.enable = true;
     end-of-file-fixer.enable = true;
-    # ripsecrets.enable = true;
+    ripsecrets.enable = true;
     # shellcheck.enable = true;
     # trim-trailing-whitespace.enable = true;
     # typos.enable = true;
