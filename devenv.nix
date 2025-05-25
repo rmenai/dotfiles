4d51acc3021223d29ed8701e5de16bf65be0f6be
Rami Menai <rami@menai.me>
2025-05-25 11:13:51 +0200

    feat: enable more pre-commits

--- devenv.nix
+++ devenv.nix
@@ -5,14 +5,14 @@
   git-hooks.hooks = {
     actionlint.enable = true;
     check-added-large-files.enable = true;
-    # check-json.enable = true;
+    check-json.enable = true;
     check-merge-conflicts.enable = true;
-    # check-yaml.enable = true;
+    check-yaml.enable = true;
     commitizen.enable = true;
     # end-of-file-fixer.enable = true;
-    ripsecrets.enable = true;
+    # ripsecrets.enable = true;
     # shellcheck.enable = true;
     # trim-trailing-whitespace.enable = true;
-    typos.enable = true;
+    # typos.enable = true;
   };
 }
