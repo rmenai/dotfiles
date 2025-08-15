c07f37f2e4e7b4f844f3eee6980cc3b1da26d992
Rami Menai <rami@menai.me>
2025-08-15 19:12:18 +0200

    feat(prism): add desktop entry for Prism Launcher

--- nixos/modules/home/apps/gaming/prism.nix
+++ nixos/modules/home/apps/gaming/prism.nix
@@ -1,9 +1,21 @@
 { config, lib, pkgs, ... }: {
   options.features.apps.gaming.prism = {
-    enable = lib.mkEnableOption "Steam gaming platform";
+    enable = lib.mkEnableOption "Prism Minecraft launcher";
   };
 
   config = lib.mkIf config.features.apps.gaming.prism.enable {
     home.packages = with pkgs; [ prismlauncher ];
+
+    xdg.desktopEntries."org.prismlauncher.PrismLauncher" = {
+      name = "Prism Launcher";
+      comment = "A custom launcher for Minecraft to multiple installations";
+      exec = "gamerun prismlauncher %U";
+      icon = "org.prismlauncher.PrismLauncher";
+      terminal = false;
+      type = "Application";
+      categories = [ "Game" "ActionGame" ];
+      mimeType = [ "application/zip" "application/x-modrinth-modpack+zip" ];
+      startupNotify = true;
+    };
   };
 }
