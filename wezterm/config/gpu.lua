5b66e0056c17c6861f58832eba9f38a70d260c02
Rami Menai <rami@menai.me>
2025-12-23 23:45:43 +0100

    fix: trying to fix wezterm gpu issue

--- wezterm/config/gpu.lua
+++ wezterm/config/gpu.lua
@@ -1,12 +1,13 @@
 local Config = {}
 
-Config.front_end = "OpenGL"
-Config.webgpu_force_fallback_adapter = false
-
 ---@diagnostic disable-next-line: undefined-field
-local battery = require("wezterm").battery_info()[1]
+-- local battery = require("wezterm").battery_info()[1]
+-- Config.webgpu_power_preference = (battery and battery.state_of_charge < 0.35) and "LowPower" or "HighPerformance"
 
-Config.webgpu_power_preference = (battery and battery.state_of_charge < 0.35) and "LowPower" or "HighPerformance"
+Config.front_end = "OpenGL"
+Config.webgpu_force_fallback_adapter = true
+Config.webgpu_power_preference = "HighPerformance"
 Config.webgpu_preferred_adapter = require("utils.gpu"):pick_best()
+Config.max_fps = 120
 
 return Config
