b8ccf0f9028a5f144341a59c7ff449920b2f28d7
Rami Menai <rami@menai.me>
2025-12-24 09:59:18 +0100

    feat: switch from pyright to ty

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -14,6 +14,14 @@ return {
   },
 
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      diagnostics = { virtual_text = false },
+    },
+  },
+
+  {
     "mrjones2014/smart-splits.nvim",
   },
 }
--- nvim/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-      diagnostics = { virtual_text = false },
-    },
-  },
-}
