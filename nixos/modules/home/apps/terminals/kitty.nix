bc1bcf1f0eef26caa2e7eb7c0aea6f0f969eb070
Rami Menai <rami@menai.me>
2025-12-26 16:20:27 +0100

    feat: migrate kitty

--- nixos/modules/home/apps/terminals/kitty.nix
+++ nixos/modules/home/apps/terminals/kitty.nix
@@ -13,8 +13,33 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.kitty ];
+    programs.kitty = {
+      enable = true;
 
-    features.core.dotfiles.links.kitty = "kitty";
+      font = {
+        name = "Iosevka Nerd Font Mono";
+        size = 12;
+      };
+
+      keybindings = {
+        "ctrl+equal" = "change_font_size all +2.0";
+        "ctrl+minus" = "change_font_size all -2.0";
+        "ctrl+0" = "change_font_size all 0";
+      };
+
+      settings = {
+        shell = "${pkgs.nushell}/bin/nu";
+        editor = "nvim";
+
+        input_delay = 0;
+        repaint_delay = 2;
+        sync_to_monitor = "no";
+        wayland_enable_ime = "no";
+
+        background_opacity = "0.9";
+      };
+    };
+
+    catppuccin.kitty.enable = true;
   };
 }
