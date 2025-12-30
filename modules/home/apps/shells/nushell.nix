fdad8cd1bce282ead6772e52fa0ee5991ec66c8d
Rami Menai <rami@menai.me>
2025-12-30 17:24:23 +0100

    feat: fix bugs in starship

--- modules/home/apps/shells/nushell.nix
+++ modules/home/apps/shells/nushell.nix
@@ -21,11 +21,23 @@ in
       starship = {
         enable = true;
         enableNushellIntegration = true;
+        settings = {
+          jobs.disabled = true; # To avoid showing atuin job
+        };
       };
 
       nushell = {
         enable = true;
         configFile.source = ./config.nu;
+
+        extraEnv = ''
+          # Use starship indicator instead
+
+          $env.PROMPT_INDICATOR = ""
+          $env.PROMPT_INDICATOR_VI_INSERT = ""
+          $env.PROMPT_INDICATOR_VI_NORMAL = ""
+          $env.PROMPT_MULTILINE_INDICATOR = "::: "
+        '';
       };
     };
 
