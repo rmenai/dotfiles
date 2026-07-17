a1fd5449e5a60bc7b85b23622c0fa6838be1d1a1
Rami Menai <rami@menai.me>
2026-07-17 20:42:28 +0200

    fix: remove fixed comment

--- hosts/kari/scratch.nix
+++ hosts/kari/scratch.nix
@@ -3,8 +3,4 @@
   #   inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default
   # ];
   #
-  # xdg.portal = {
-  #   enable = true;
-  #   extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  # };
 }
