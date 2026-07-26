49dd7dd8d751c08cff4320965e03c2d856426cfb
Rami Menai <rami@menai.me>
2026-07-26 18:10:46 +0200

    feat: migrate cloudflare ip restriction to caddy instead

--- hosts/kagi/networking.nix
+++ hosts/kagi/networking.nix
@@ -4,35 +4,6 @@
   pkgs,
   ...
 }:
-let
-  cloudflareIpsV4 = [
-    "173.245.48.0/20"
-    "103.21.244.0/22"
-    "103.22.200.0/22"
-    "103.31.4.0/22"
-    "141.101.64.0/18"
-    "108.162.192.0/18"
-    "190.93.240.0/20"
-    "188.114.96.0/20"
-    "197.234.240.0/22"
-    "198.41.128.0/17"
-    "162.158.0.0/15"
-    "104.16.0.0/13"
-    "104.24.0.0/14"
-    "172.64.0.0/13"
-    "131.0.72.0/22"
-  ];
-
-  cloudflareIpsV6 = [
-    "2400:cb00::/32"
-    "2606:4700::/32"
-    "2803:f800::/32"
-    "2405:b500::/32"
-    "2405:8100::/32"
-    "2a06:98c0::/29"
-    "2c0f:f248::/32"
-  ];
-in
 {
   sops.secrets = {
     "secrets/wireguard_private_key" = {
@@ -56,22 +27,33 @@ in
       enable = true;
       allowPing = false;
 
-      trustedInterfaces = [
-        "tailscale0"
-        "wg0"
-      ];
-
       allowedTCPPorts = lib.mkForce [ ];
+      allowedUDPPorts = lib.mkForce [ ];
 
-      allowedUDPPorts = lib.mkForce [
-        41641 # Tailscale
-        51820 # Wireguard
-      ];
+      extraCommands = ''
+        # Netavark firewall rules
+        # HTTP (80) and HTTPS (443)
 
-      # Whitelist http traffic only for cloudflare ip ranges
-      extraInputRules = ''
-        ${lib.concatMapStringsSep "\n" (ip: "ip saddr ${ip} tcp dport { 80, 443 } accept") cloudflareIpsV4}
-        ${lib.concatMapStringsSep "\n" (ip: "ip6 saddr ${ip} tcp dport { 80, 443 } accept") cloudflareIpsV6}
+        # Allow loopback traffic
+        iptables -A INPUT -i lo -j ACCEPT
+        ip6tables -A INPUT -i lo -j ACCEPT
+
+        # Allow established and related outgoing traffic to return safely
+        iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+        ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+        # Tailscale (41641)
+        iptables -A INPUT -p udp -m multiport --dports 41641 -j ACCEPT
+        ip6tables -A INPUT -p udp -m multiport --dports 41641 -j ACCEPT
+
+        # Allow Tailscale interface completely
+        iptables -A INPUT -i tailscale0 -j ACCEPT
+        ip6tables -A INPUT -i tailscale0 -j ACCEPT
+      '';
+
+      extraStopCommands = ''
+        iptables -F INPUT || true
+        ip6tables -F INPUT || true
       '';
     };
   };
