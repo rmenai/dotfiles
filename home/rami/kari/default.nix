1204266ef7364f3f46845b8987b2a126a6438f22
Rami Menai <rami@menai.me>
2026-08-07 11:44:32 +0200

    feat: fix calibre plugin installation failure

--- home/rami/kari/default.nix
+++ home/rami/kari/default.nix
@@ -70,6 +70,14 @@
     keepassxc
     rustdesk-flutter
     kdePackages.dolphin
+
+    (pkgs.calibre.overrideAttrs (old: {
+      postInstall = ''
+        wrapProgram $out/bin/calibre \
+            --set-default ACSM_LIBCRYPTO ${pkgs.openssl.out}/lib/libcrypto.so \
+            --set-default ACSM_LIBSSL ${pkgs.openssl.out}/lib/libssl.so
+      '';
+    }))
     # thunderbird
     # protonmail-bridge
 
